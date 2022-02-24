3e81c7c66cec5b32945c7c9c1ee2f76f6acb87cd
Murari Gupta <34083708+Ariesleo@users.noreply.github.com>
2022-02-24 21:55:26 +0545

    made a client side request to send the data and send the response using the fetch api method

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ app.listen(3000, () => {
 })
 
 app.use(express.static('public'))
+
+app.use(express.json({ limit: '1mb' }))
+
+app.post('/api', (request, response) => {
+    console.log('I got request')
+    console.log(request.body)
+    response.json({
+        status: 'success',
+        latitude: request.body.latitude,
+        longitude: request.body.longitude,
+    })
+})
